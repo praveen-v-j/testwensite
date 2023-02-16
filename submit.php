1100a46857a8b8f99416c6d41cfe87c597120f1a
unknown <praveen06506@gmail.com>
2023-02-16 14:13:26 +0530

    sad

--- submit.php
+++ submit.php
@@ -1,44 +1,44 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-
-require_once 'assets\PHPMailer-master\src\Exception.php';
-require_once 'assets\PHPMailer-master\src\PHPMailer.php';
-require_once 'assets\PHPMailer-master\src\SMTP.php';
-
-$mail = new PHPMailer(true);
-
-$alert = '';
-
-if(isset($_POST['send'])){
+if (isset($_POST['send'])) {
+  // Get the form data
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
 
-  try{
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'praveen3164@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'nzkxcbqrmpmhfafo'; // Gmail address Password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
+  // Load the Google Sheets API library
+  require_once __DIR__ . 'assets\vendor\autoload.php';
 
-    $mail->setFrom('praveen3164@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('praveen06506@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+  // Authenticate with the Google Sheets API
+  putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . 'assets\js\blissful-hash-337610-e2e3d621e636.json');
+  $client = new Google_Client();
+  $client->useApplicationDefaultCredentials();
+  $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Message Received (Contact Page)';
-    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
+  // Connect to the Google Sheet
+  $service = new Google_Service_Sheets($client);
+  $spreadsheetId = 'https://docs.google.com/spreadsheets/d/1BLARFUPNUqUXnvM9k4fb9n11fl38GRbWXLww3Iz94Mc';
+  $range = 'Sheet1!A:C';
 
-    $mail->send();
-    $alert = '<div class="alert-success">
-                 <span>Message Sent! Thank you for contacting us.</span>
-                </div>';
-  } catch (Exception $e){
-    $alert = '<div class="alert-error">
-                <span>'.$e->getMessage().'</span>
-              </div>';
-  }
+  // Get the last row in the Google Sheet
+  $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+  $values = $response->getValues();
+  $nextRow = count($values) + 1;
+
+  // Write the form data to the Google Sheet
+  $values = [
+    [$name, $email, $message],
+  ];
+  $body = new Google_Service_Sheets_ValueRange([
+    'values' => $values,
+  ]);
+  $params = [
+    'valueInputOption' => 'RAW',
+    'insertDataOption' => 'INSERT_ROWS',
+  ];
+  $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+  // Redirect to a thank-you page
+  header('Location: #');
+  exit;
 }
 ?>
-      
